9d59de30ae02510f30774abc8f7b8c2f46f94d36
vlad <vladhuman1982@gmail.com>
2020-12-14 15:33:13 +0200

    modal1 end

--- js/task-4.js
+++ js/task-4.js
@@ -6,16 +6,14 @@ let input = prompt('Сколько вы хотите купить дроидов
 
 let totalPrice;
 
-if (input === null) {
+if (!input) {
     console.log('Отменено пользователем!');
-} else if (input <= 0) {
-    console.log('Вы не выбрали количество дроидов');
 } else {
     totalPrice = pricePerDroid * input;
-}
 if (totalPrice > credits) {
     console.log('Недостаточно средств на счету!')
 } else {
     credits -= totalPrice;
     console.log(`Вы купили ${input} дроидов, на счету осталось ${credits} кредитов.`);
+}
 }
